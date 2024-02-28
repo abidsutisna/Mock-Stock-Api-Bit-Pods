0319102c07e56936a2cb5c8d4702a0436f725eff
Vincent <vincentkho67@gmail.com>
2024-02-28 16:21:09 +0700

    [FEATURE] done

---
+++ src/main/java/bytebrewers/mockbitpodsstockservice/model/Stock.java
@@ -0,0 +1,14 @@
+package bytebrewers.mockbitpodsstockservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Stock {
+    private String symbol;
+    private Company company;
+    private double close;
+    private double change;
+    private double percent;
+}
